076c6d3c3595be873baaf09ebca3e46cd4e308ca
SivamPrasath <sivamprasathk@gmail.com>
2024-04-30 12:57:25 +0300

    Initialization

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "This is the Home Page"
+
 if __name__ == '_main_':
-    app.run(debug=True, port=8000)
-    
+    app.run(debug=True, port=5000)
